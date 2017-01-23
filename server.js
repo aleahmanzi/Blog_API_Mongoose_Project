9ab94c7492ed2000d0595d24394d5d0ad12467d4
Aleah Manzi <aleah.manzi@gmail.com>
2017-01-23 18:16:36 -0500

    syntax updates

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const blogPostRouter = requre('./blogPostRouter')
+const blogPostRouter = require('./blogPostRouter')
 
 mongoose.Promise = global.Promise;
 
@@ -10,8 +10,6 @@ const {blogPost} = require('./models');
 
 const app = express();
 app.use(bodyParser.json());
-
-mongoose.Promise = global.Promise;
 
 let server;
 
